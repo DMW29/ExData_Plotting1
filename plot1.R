0d0d739d08dcf0bd8c7522000c85ab74c8c267b8
DMW29 <watersod@gmail.com>
2015-09-10 17:38:32 -0600

    added closeAllConnections

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
-
+## Plot 1
+## ----------------------------------------------------------------------------
 ## read.csv.sql will be used to read in data from February 1, 2007 and 
 ## February 2, 2007. The data set is assumed to be located within the current
 ## working directory. Functions with() and hist() will be used to create 
@@ -12,14 +13,15 @@ wd <- getwd()
 if(!is.null(wd)){setwd(wd)}
 filename <- "./household_power_consumption.txt"
 
-## Read in data.
+## Read in data and close connections to avoid warnings.
 epc <- read.csv.sql(filename, sql = "select * from file where Date in 
                     ('1/2/2007', '2/2/2007')", header = TRUE, sep = ";")
+closeAllConnections()
 
 ## Create Global Active Power histogram.
 with(epc, hist(Global_active_power, col = "red", main = "Global Active Power", 
                xlab = "Global Active Power (kilowatts)", ylab = "Frequency"))
 
 ## Create the .png file and close the graphics device.
-dev.copy(png, file = "plot 1")
+dev.copy(png, file = "Plot 1")
 dev.off()
